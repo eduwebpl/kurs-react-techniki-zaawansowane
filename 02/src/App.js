875c5f4ae33341a6af511e1e5e1827f0db18bcdc
Adam Romański <adam.romanski@netguru.pl>
2019-05-08 21:26:38 +0200

    02

---
+++ 02/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./App.module.scss";
+import cx from "classnames";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
