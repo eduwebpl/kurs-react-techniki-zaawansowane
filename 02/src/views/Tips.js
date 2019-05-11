3e57a712a7bf00a00b2536ea06c57926fbe1069e
Adam Romański <adam.romanski@netguru.pl>
2019-05-11 21:15:36 +0200

    02.3-finish

--- 02/src/views/Tips.js
+++ 02/src/views/Tips.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Columns from "../components/Columns/Columns";
 
 const Tips = () => (
   <div>
     <h2 className="title is-3">Tips</h2>
+    <Columns />
   </div>
 );
 
