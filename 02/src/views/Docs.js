27779aeb95872915c8fb1946f46cde7a5e14001a
Adam Romański <adam.romanski@netguru.pl>
2019-05-11 19:56:43 +0200

    02.3

---
+++ 02/src/views/Docs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ItemsList from "../components/ItemsList/ItemsList";
+
+const Docs = () => (
+  <div>
+    <h2 className="title is-3">Docs</h2>
+    <ItemsList />
+  </div>
+);
+
+export default Docs;
