d17cd2b695cd32d67aee325d8dd77df1b7537e9b
Adam Romański <adam.romanski@netguru.pl>
2019-05-17 22:59:19 +0200

    02.7-finish

--- 02/src/views/Patterns.js
+++ 02/src/views/Patterns.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+const MyComponent = props => (
+  <div>
+    <p>Hello world</p>
+    {props.render("Roman")}
+  </div>
+);
+
 const Patterns = () => (
   <div>
     <h2 className="title is-3">Patterns</h2>
+    <MyComponent render={data => <p>Hello {data}</p>} />
   </div>
 );
 
