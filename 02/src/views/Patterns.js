5cff5336a6627acecd9d172021f6f8475efad377
Adam Romański <adam.romanski@netguru.pl>
2019-05-18 20:30:25 +0200

    02.10-finish

--- 02/src/views/Patterns.js
+++ 02/src/views/Patterns.js
@@ -1,8 +1,25 @@
 import React from "react";
 
+const items = [
+  { value: "apple" },
+  { value: "pear" },
+  { value: "orange" },
+  { value: "grape" },
+  { value: "banana" },
+];
+
 const Patterns = () => (
-  <div>
-    <h2 className="title is-3">Patterns</h2>
+  <div className="dropdown is-active">
+    <input className="input" type="text" placeholder="Text input" />
+    <div className="dropdown-menu">
+      <div className="dropdown-content">
+        {items.map(({ value }) => (
+          <button className="dropdown-item button is-white" key={value}>
+            {value}
+          </button>
+        ))}
+      </div>
+    </div>
   </div>
 );
 
