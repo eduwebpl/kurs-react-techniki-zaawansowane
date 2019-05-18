365d63fca97d838936f4a95d40099d5e2b442b9b
Adam Romański <adam.romanski@netguru.pl>
2019-05-18 21:23:01 +0200

    02.11-finish

--- 02/src/views/Patterns.js
+++ 02/src/views/Patterns.js
@@ -1,4 +1,6 @@
 import React from "react";
+import cx from "classnames";
+import Downshift from "downshift";
 
 const items = [
   { value: "apple" },
@@ -9,18 +11,46 @@ const items = [
 ];
 
 const Patterns = () => (
-  <div className="dropdown is-active">
-    <input className="input" type="text" placeholder="Text input" />
-    <div className="dropdown-menu">
-      <div className="dropdown-content">
-        {items.map(({ value }) => (
-          <button className="dropdown-item button is-white" key={value}>
-            {value}
-          </button>
-        ))}
+  <Downshift>
+    {({
+      isOpen,
+      inputValue,
+      getInputProps,
+      getMenuProps,
+      getItemProps,
+      selectedItem,
+    }) => (
+      <div className={cx("dropdown", { "is-active": isOpen })}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Text input"
+          {...getInputProps()}
+        />
+        <div className="dropdown-menu">
+          <div className="dropdown-content" {...getMenuProps()}>
+            {items
+              .filter(item => item.value.includes(inputValue))
+              .map(({ value }, index) => (
+                <button
+                  className={cx("dropdown-item", "button", "is-white", {
+                    "is-active": value === selectedItem,
+                  })}
+                  key={value}
+                  {...getItemProps({
+                    key: value,
+                    index,
+                    item: value,
+                  })}
+                >
+                  {value}
+                </button>
+              ))}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    )}
+  </Downshift>
 );
 
 export default Patterns;
