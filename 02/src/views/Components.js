52e6dd870fbf50c125b3dc809e838b77574a7934
Adam Romański <adam.romanski@netguru.pl>
2019-05-23 20:53:31 +0200

    02.12-finish

--- 02/src/views/Components.js
+++ 02/src/views/Components.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Components = () => (
-  <div>
-    <h2 className="title is-3">Components</h2>
-  </div>
-);
+const Components = () => {
+  const [inputContent, setInputContent] = useState("Hello World");
+
+  const handleInputChange = e => {
+    setInputContent(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2 className="title is-3">Components</h2>
+      <p>Hi there, {inputContent}</p>
+      <input name="name" onChange={handleInputChange} />
+    </div>
+  );
+};
 
 export default Components;
