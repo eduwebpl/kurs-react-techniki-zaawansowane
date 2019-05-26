053755fe20534c1243bff92ef01ce9582132f788
Adam Romański <adam.romanski@netguru.pl>
2019-05-26 15:39:00 +0200

    02.16-finish

--- 02/src/views/Components.js
+++ 02/src/views/Components.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const Components = () => (
-  <div>
-    <h2 className="title is-3">Components</h2>
-  </div>
-);
+const style = {
+  transition: "transform 1s ease-in",
+  width: "100px",
+  transformOrigin: "0% 50%",
+  display: "block",
+};
+
+const Components = () => {
+  const myInputRef = useRef(null);
+
+  const handleClick = () => {
+    myInputRef.current.focus();
+    myInputRef.current.style.transform = "scaleX(2)";
+  };
+
+  return (
+    <div>
+      <h2 className="title is-3">Components</h2>
+      <input style={style} ref={myInputRef} />
+      <button onClick={handleClick}>focus input</button>
+    </div>
+  );
+};
 
 export default Components;
