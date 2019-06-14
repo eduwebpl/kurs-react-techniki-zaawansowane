842c7cddd763db3370a8a756a71c69ff63cc6617
Adam Romański <adam.romanski@netguru.pl>
2019-06-14 22:28:56 +0200

    02.18-finish

--- 02/src/views/Tips.js
+++ 02/src/views/Tips.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Tips = () => (
-  <div>
-    <h2 className="title is-3">Tips</h2>
-  </div>
-);
+const Tips = () => {
+  const [isTooltipOpen, setTooltipVisibility] = useState(false);
+
+  return (
+    <div>
+      <h2 className="title is-3">Tips</h2>
+      <button
+        className="button is-large is-primary"
+        onClick={() => setTooltipVisibility(!isTooltipOpen)}
+      >
+        Open tooltip
+      </button>
+      {isTooltipOpen && (
+        <div class="notification is-primary">
+          Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem
+          ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis
+          placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+          fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a>{" "}
+          efficitur. Sit amet, consectetur adipiscing elit
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Tips;
--- 02/src/views/Users.js
+++ 02/src/views/Users.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import cx from "classnames";
 
-const Users = () => (
-  <div>
-    <h2 className="title is-3">Users</h2>
-  </div>
-);
+const Users = () => {
+  const [isModalVisible, setModalVisibility] = useState(false);
+
+  return (
+    <div>
+      <h2 className="title is-3">Users</h2>
+      <button
+        className="button is-primary"
+        onClick={() => setModalVisibility(true)}
+      >
+        Open modal
+      </button>
+      <div className={cx("modal", { "is-active": isModalVisible })}>
+        <div className="modal-background" />
+        <div className="modal-content">
+          <article class="message">
+            <div class="message-header">
+              <p>Hello World</p>
+              <button
+                class="delete"
+                aria-label="delete"
+                onClick={() => setModalVisibility(false)}
+              />
+            </div>
+            <div class="message-body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
+              porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla.
+            </div>
+          </article>
+        </div>
+        <button className="modal-close is-large" aria-label="close" />
+      </div>
+    </div>
+  );
+};
 
 export default Users;
