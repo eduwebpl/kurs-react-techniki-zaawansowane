029989b9a714516bee55cd09f01d79385f946428
Adam Romański <adam.romanski@netguru.pl>
2019-06-18 21:36:49 +0200

    03-finish

--- 03/src/views/Users.js
+++ 03/src/views/Users.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+
+const MyContext = React.createContext();
+
+const User = () => {
+  const context = useContext(MyContext);
+
+  return (
+    <div>
+      <p>User name: {context.name}</p>
+    </div>
+  );
+};
 
 const Users = () => (
   <div>
-    <h2 className="title is-3">Users</h2>
+    <MyContext.Provider value={{ name: "Roman" }}>
+      <h2 className="title is-3">Users</h2>
+      <User />
+    </MyContext.Provider>
   </div>
 );
 
