5f33454874aae12fc7f318d57a445da90a7ebcb5
Adam Romański <adam.romanski@netguru.pl>
2019-06-18 23:29:12 +0200

    03.3-finish

--- 03/src/components/MultiStep/MultiStep.js
+++ 03/src/components/MultiStep/MultiStep.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 
-const Page = ({ children }) => <div>{children}</div>;
+const WizardContext = React.createContext({
+  currentPage: 1,
+  changePage: () => {},
+});
 
-const Controls = () => (
-  <div>
-    <button>Previous</button>
-    <button>Next</button>
-    {/* <buton>Submit</buton> */}
-  </div>
-);
+const Page = ({ children, pageIndex }) => {
+  const { currentPage } = useContext(WizardContext);
+
+  return currentPage === pageIndex ? children : null;
+};
+
+const Controls = props => {
+  const { changePage, currentPage } = useContext(WizardContext);
+
+  return (
+    <div {...props}>
+      <button
+        className="button is-primary is-small"
+        onClick={() => changePage(currentPage - 1)}
+      >
+        Previous
+      </button>
+      <button
+        className="button is-warning is-small"
+        onClick={() => changePage(currentPage + 1)}
+      >
+        Next
+      </button>
+      {/* <buton>Submit</buton> */}
+    </div>
+  );
+};
 
 const Wizard = ({ children }) => {
-  return <div>{children}</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const changePage = newPageIndex => {
+    setCurrentPage(newPageIndex);
+  };
+
+  return (
+    <WizardContext.Provider
+      value={{
+        currentPage,
+        changePage,
+      }}
+    >
+      {children}
+    </WizardContext.Provider>
+  );
 };
 
 export { Page, Controls, Wizard };
--- 03/src/views/Users.js
+++ 03/src/views/Users.js
@@ -2,14 +2,30 @@ import React from "react";
 import * as MultiStep from "components/MultiStep/MultiStep";
 
 const Users = () => (
-  <div>
-    <p>Users</p>
-    <MultiStep.Wizard>
-      <MultiStep.Controls />
-      <MultiStep.Page>Page 1</MultiStep.Page>
-      <MultiStep.Page>Page 2</MultiStep.Page>
-      <MultiStep.Page>Page 3</MultiStep.Page>
-    </MultiStep.Wizard>
+  <div className="columns is-centered">
+    <div className="column is-half">
+      <MultiStep.Wizard>
+        <MultiStep.Page pageIndex={1}>
+          <div className="box">
+            <p className="title">What's your name</p>
+            <input type="text" name="name" placeholder="name" />
+          </div>
+        </MultiStep.Page>
+        <MultiStep.Page pageIndex={2}>
+          <div className="box">
+            <p className="title">And your last name?</p>
+            <input type="text" name="lastName" placeholder="last name" />
+          </div>
+        </MultiStep.Page>
+        <MultiStep.Page pageIndex={3}>
+          <div className="box">
+            <p className="title">What about your phone number?</p>
+            <input type="text" name="phone" placeholder="phone" />
+          </div>
+        </MultiStep.Page>
+        <MultiStep.Controls className="columns is-centered" />
+      </MultiStep.Wizard>
+    </div>
   </div>
 );
 
