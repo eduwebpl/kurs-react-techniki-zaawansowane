054b8acca814ec1267b2548667910e7d7b2813ab
Adam Romański <adam.romanski@netguru.pl>
2019-06-20 19:31:10 +0200

    03.4-finish

--- 03/src/components/MultiStep/MultiStep.js
+++ 03/src/components/MultiStep/MultiStep.js
@@ -1,40 +1,59 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 const WizardContext = React.createContext({
   currentPage: 1,
   changePage: () => {},
+  pageIndexes: [],
+  updatePageIndexes: () => {},
 });
 
 const Page = ({ children, pageIndex }) => {
-  const { currentPage } = useContext(WizardContext);
+  const { currentPage, updatePageIndexes } = useContext(WizardContext);
+
+  useEffect(() => {
+    updatePageIndexes(pageIndex);
+  });
 
   return currentPage === pageIndex ? children : null;
 };
 
 const Controls = props => {
-  const { changePage, currentPage } = useContext(WizardContext);
+  const { changePage, currentPage, pageIndexes } = useContext(WizardContext);
 
   return (
     <div {...props}>
       <button
+        disabled={currentPage === 1}
         className="button is-primary is-small"
         onClick={() => changePage(currentPage - 1)}
       >
         Previous
       </button>
       <button
+        disabled={currentPage === pageIndexes.length}
         className="button is-warning is-small"
         onClick={() => changePage(currentPage + 1)}
       >
         Next
       </button>
-      {/* <buton>Submit</buton> */}
+      {currentPage === pageIndexes.length ? (
+        <button className="button is-info is-small">Submit</button>
+      ) : null}
     </div>
   );
 };
 
 const Wizard = ({ children }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [pageIndexes, setPageIndexes] = useState([]);
+
+  const updatePageIndexes = pageIndex => {
+    if (pageIndexes.includes(pageIndex)) {
+      return;
+    }
+
+    setPageIndexes([...pageIndexes, pageIndex]);
+  };
 
   const changePage = newPageIndex => {
     setCurrentPage(newPageIndex);
@@ -45,6 +64,8 @@ const Wizard = ({ children }) => {
       value={{
         currentPage,
         changePage,
+        pageIndexes,
+        updatePageIndexes,
       }}
     >
       {children}
