4be4a0c587d99ac1479297964aac1621c8530ada
Adam Romański <adam.romanski@netguru.pl>
2019-06-27 21:57:42 +0200

    04.4-finish

--- 04/src/components/Input/Input.js
+++ 04/src/components/Input/Input.js
@@ -1,5 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Input = () => <input placeholder="Name" />;
+const Input = ({ placeholder }) => <input placeholder={placeholder} />;
+
+Input.propTypes = {
+  placeholder: PropTypes.string,
+};
+
+Input.defaultProps = {
+  placeholder: "Your Value",
+};
 
 export default Input;
--- 04/src/components/Input/Input.test.js
+++ 04/src/components/Input/Input.test.js
@@ -4,8 +4,17 @@ import Input from "./Input";
 
 describe("Input component", () => {
   it("renders input element", () => {
+    const placeholderText = "First Name";
+    const { getByPlaceholderText } = render(
+      <Input placeholder={placeholderText} />
+    );
+
+    expect(getByPlaceholderText(placeholderText)).toBeInTheDocument();
+  });
+  it("displays default placeholder", () => {
+    const defaultPlaceholderText = "Your Value";
     const { getByPlaceholderText } = render(<Input />);
 
-    expect(getByPlaceholderText(/name/i)).toBeInTheDocument();
+    expect(getByPlaceholderText(defaultPlaceholderText)).toBeInTheDocument();
   });
 });
