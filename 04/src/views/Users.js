d299653066717ebe8ba2d3b9a4408c4b00a03821
Adam Romański <adam.romanski@netguru.pl>
2019-07-02 22:32:37 +0200

    04.11-finish

--- 04/src/views/Users.js
+++ 04/src/views/Users.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-const Users = () => (
-  <div>
-    <h2 className="title is-3">Users</h2>
-  </div>
-);
+const Users = () => {
+  const [usersData, setUsersData] = useState([]);
+  
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const { data } = await axios.get('http://www.mocky.io/v2/5d1bb9033400003700000934?mocky-delay=300ms');
+        
+        setUsersData(data);
+      }
+      catch (err) {
+        console.log(err);
+      }
+      
+    }
+    fetchUsers();
+  });
+  
+  return (
+    <div>
+      <h2 className="title is-3">Users</h2>
+      <div>
+        {
+          usersData.length === 0 ? (
+            <p>Loading users...</p>
+          ) : (
+            usersData.map(({name, age}) => <p key={name}>{name} is {age} years old</p>)
+          )
+        }
+      </div>
+    </div>
+  )   
+};
 
 export default Users;
+ 
--- 04/src/views/__tests__/Users.test.js
+++ 04/src/views/__tests__/Users.test.js
@@ -3,5 +3,9 @@ import { render } from '@testing-library/react';
 import Users from 'views/Users';
 
 describe('Users view', () => {
-  it('')
+  it('displays loading indicator', () => {
+    const { getByText } = render(<Users />);
+    
+    expect(getByText(/loading/i)).toBeInTheDocument();
+  })
 })
